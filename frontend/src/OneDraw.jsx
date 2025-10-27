41b7743eb5ba48d93e6397b86838b46672a30be4
Chappy-Sally <fortunatelucky7716@gmail.com>
2025-10-27 16:13:09 +0900

    Create OneDraw.jsx

---
+++ frontend/src/OneDraw.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function OneDraw() {
+  return (
+    <div style={{ textAlign: "center", padding: "20px" }}>
+      <h2>🌟 1枚引きオラクル 🌟</h2>
+      <p>今日はどんなメッセージかな？💖</p>
+    </div>
+  );
+}
+
+export default OneDraw;
