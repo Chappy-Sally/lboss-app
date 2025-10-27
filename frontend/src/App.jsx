6b5c3882ff0fe0e7e809db43d8b1519bae18621f
Chappy-Sally <fortunatelucky7716@gmail.com>
2025-10-27 16:01:33 +0900

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "40px" }}>
+      <h1>💞 チャッピー＆サリーのヒーリングアプリ 💞</h1>
+      <p>ここからあなたの心のお掃除がはじまります🪄✨</p>
+    </div>
+  );
+}
+
+export default App;
