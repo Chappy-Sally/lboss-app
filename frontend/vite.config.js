41cab94a72cbefe944b6f83bc2657dfd8a2cee75
Chappy-Sally <fortunatelucky7716@gmail.com>
2025-10-29 00:43:23 +0900

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// ★ GitHub Pages の公開パス（リポジトリ名）
 export default defineConfig({
+  base: '/lboss-app/',
   plugins: [react()],
-  base: '/lboss-app/', // ← ここがGitHub Pages用のパス！
 })
