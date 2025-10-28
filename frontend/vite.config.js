8f70cc798bca81a20351562b9f0d50999455513d
Chappy-Sally <fortunatelucky7716@gmail.com>
2025-10-29 08:03:56 +0900

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,8 @@
+// frontend/vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/lboss-app/',
+  base: '/lboss-app/', // ← ここが超重要（リポジトリ名）
   plugins: [react()],
 })
