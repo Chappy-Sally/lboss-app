f166cec5c0b7678abb08cab5c0ca72b474cf34fb
Chappy-Sally <fortunatelucky7716@gmail.com>
2025-10-29 08:54:38 +0900

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
-  <HashRouter>
+  <BrowserRouter basename="/lboss-app">
     <App />
-  </HashRouter>
+  </BrowserRouter>
 );
